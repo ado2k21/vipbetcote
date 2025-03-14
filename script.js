5b678d7beac9a6fa9ae14672b157b3423141604e
admin <reoj999@gmail.com>
2025-03-14 12:09:32 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -30,3 +30,43 @@ function prevSlide() {
 document.addEventListener('DOMContentLoaded', () => {
   showSlide(currentSlide);
 });
+
+// Liste des messages publicitaires
+const messages = [
+  "Nouvel achat plan 30jou",
+  "Nouvel achat plan 5jou",
+  "Nouvel achat plan 5jou",
+  "9 Nouvel achat plan 15jou",
+  "Nouvel achat plan a Vie",
+  "Nouvel achat plan a Vie",
+  "Nouvel achat plan 5jou",
+  "239 Nouvel achat jodia",
+  "Nouvel achat plan a Vie",
+  "Cote 248 valider hier",
+  "Nouvel achat plan 15jou",
+  "Nouvel achat plan 5jou",
+  "Nouvel achat plan a Vie",
+  "Nouvel achat plan 5jou",
+  "Yon abòne fè 33000gdes avanhier"
+];
+
+// Fonction pour afficher un message aléatoire
+function afficherPub() {
+  const pubText = document.getElementById("pub-text");
+  const randomIndex = Math.floor(Math.random() * messages.length);
+  pubText.textContent = messages[randomIndex];
+
+  // Afficher le texte
+  pubText.classList.add("show");
+
+  // Disparaître après 3 secondes
+  setTimeout(() => {
+    pubText.classList.remove("show");
+  }, 3000);
+}
+
+// Afficher la première pub après 15 secondes
+setTimeout(afficherPub, 15000);
+
+// Afficher une pub toutes les 25 secondes après la première
+setInterval(afficherPub, 25000);
